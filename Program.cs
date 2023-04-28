ae95959705fc512ca7c15b93b000e62b9cfabd44
Robert Burston <ST10086237@imconnect.edu.za>
2023-04-28 13:27:15 +0200

    Comments
    Extensive comments added.

--- Program.cs
+++ Program.cs
@@ -8,86 +8,119 @@ namespace POEPart1
         // Main method
         static void Main(string[] ags)
         {
+            // set console color to cyan
             Console.ForegroundColor = ConsoleColor.Cyan;
+            // print welcome message to the console
             Console.WriteLine($"Welcome to RecipeApp!");
+            // reset console color
             Console.ResetColor();
 
-            Recipe recipe = null; // Initialize the recipe object to null 
-            bool exit = false; // Initialize the exit flag to false
+            // Initialize the recipe object to null 
+            Recipe recipe = null;
+            // Initialize the exit flag to false
+            bool exit = false;
 
-            while (!exit) // Loop until the exit flag is set to true
+            // Loop until the exit flag is set to true
+            while (!exit) 
             {
+                // set console color to white
                 Console.ForegroundColor = ConsoleColor.White;
+                // print prompt to the console
                 Console.WriteLine("Enter a command (new, display, scale, reset, clear, exit):");
+                // reset console color
                 Console.ResetColor();
-
-                string command = Console.ReadLine(); // Read the user's command input
+                // Read the user's command input
+                string command = Console.ReadLine(); 
 
                 switch (command)
                 {
                     case "new":
+                        // create a new recipe
+                        // set console color to cyan
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("Enter the name of the recipe: ");
+                        // reset console color
                         Console.ResetColor();
+                        // read recipe name from console input
                         string rName = Console.ReadLine();
                         
+                        // set console color to cyan
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("Enter the number of ingredients:");
+                        // reset console color
                         Console.ResetColor();
+                        // read number of ingredients from console input
                         int numIngredients;
                         string line = Console.ReadLine();
                         
+                        // try to parse number of ingredients from the input string
                         try
                         {
-                           numIngredients = Int32.Parse(line);
+                            numIngredients = Int32.Parse(line);
                         }
                         catch (FormatException)
                         {
+                            // print error message if the input is not a number and go back to the "new" case
                             Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("{0} is not an number!", line);
+                            Console.WriteLine("{0} is not a number!", line);
+                            // reset console color
                             Console.ResetColor();
                             goto case "new";
                         }
 
+                        // set console color to cyan
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("Enter the number of steps:");
+                        // reset console color
                         Console.ResetColor();
+                        // read number of steps from console input
                         int numSteps;
                         string inputSteps = Console.ReadLine();
                         
+                        // try to parse number of steps from the input string
                         try
                         {
                             numSteps = Int32.Parse(inputSteps);
                         }
                         catch (FormatException)
                         {
+                            // print error message if the input is not a number and go back to the "new" case
                             Console.ForegroundColor = ConsoleColor.Red;
                             Console.WriteLine("{0} is not a number!", inputSteps);
+                            // reset console color
                             Console.ResetColor();
                             goto case "new";
                         }
 
+                        // create a new Recipe object with the given number of ingredients and steps
                         recipe = new Recipe(numIngredients, numSteps);
     
+                        // loop over the number of ingredients to get their details
                         for (int i = 0; i < numIngredients; i++)
                         {
+                            // set console color to cyan
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine($"Enter ingredient {i + 1} name:");
+                            // reset console color
                             Console.ResetColor();
+                            // read ingredient name from console input
                             string name = Console.ReadLine();
 
                             Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine($"Enter ingredient {i + 1} quantity:");
+                            Console.WriteLine($"Enter ingredient {i + 1} quantity:"); // Prompts user to enter ingredient quantity
                             Console.ResetColor();
                             decimal quantity;
                             string inputQuantity = Console.ReadLine();
-
+                            
+                            // try to parse number ingredients from the input string
                             try
                             {
                                 quantity = decimal.Parse(inputQuantity);
                             }
+                            
                             catch (FormatException)
                             {
+                                // print error message if the input is not a number and go back to the "new" case
                                 Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("{0} is not a number!", inputQuantity);
                                 Console.ResetColor();
@@ -103,25 +136,30 @@ namespace POEPart1
                             recipe.SetIngredient(i, name, quantity, unit);
                         }
 
-                        for (int i = 0; i < numSteps; i++)
+                        // Iterate through each recipe step and prompt user to enter a description
+                        for (int i = 0; i < numSteps; i++) 
                         {
+                            // Set console color to cyan and display step number prompt
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine($"Enter step {i + 1} description:");
                             Console.ResetColor();
+
+                            // Read user input for step description and store in variable
                             string description = Console.ReadLine();
 
+                            // Update recipe step with description
                             recipe.SetStep(i, description);
                         }
-
+                        
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Recipe created!");
                         Console.ResetColor();
                         break;
 
                     case "display":
-                        if (recipe != null)
+                        if (recipe != null) // If recipe is not null run the method call
                         {
-                            recipe.Display();
+                            recipe.Display(); // method call from recipe class
                         }
                         else
                         {
@@ -133,7 +171,7 @@ namespace POEPart1
                         break;
 
                    case "scale":
-                        if (recipe != null)
+                        if (recipe != null) // If recipe is not null run the method call
                         {
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine("Enter the scale: (half, double, triple)");
@@ -143,7 +181,7 @@ namespace POEPart1
                             {
                                 case "half":
                                 {
-                                    recipe.Scale(0.5);
+                                    recipe.Scale(0.5); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe scaled by a factor of 0,5 or half.");
                                     Console.ResetColor();
@@ -152,7 +190,7 @@ namespace POEPart1
 
                                 case "double":
                                 {
-                                    recipe.Scale(2);
+                                    recipe.Scale(2); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe scaled by a factor of 2 or double.");
                                     Console.ResetColor();
@@ -161,7 +199,7 @@ namespace POEPart1
 
                                 case "triple":
                                 {
-                                    recipe.Scale(3);
+                                    recipe.Scale(3); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe scaled by a factor of 3 or triple.");
                                     Console.ResetColor();
@@ -171,7 +209,7 @@ namespace POEPart1
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Red; // If recipe is null run this error message
                             Console.WriteLine("Can't scale when there are no quantities to work with!");
                             Console.ResetColor();
                         }
@@ -191,7 +229,7 @@ namespace POEPart1
                             {
                                 case "half":
                                 {
-                                    recipe.Reset(0.5);
+                                    recipe.Reset(0.5); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe divided/reset by 0,5 or half.");
                                     Console.ResetColor();
@@ -200,7 +238,7 @@ namespace POEPart1
 
                                 case "double":
                                 {
-                                    recipe.Reset(2);
+                                    recipe.Reset(2); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe divided/reset by 2 or double.");
                                     Console.ResetColor();
@@ -209,7 +247,7 @@ namespace POEPart1
 
                                 case "triple":
                                 {
-                                    recipe.Reset(3);
+                                    recipe.Reset(3); // method call from recipe class
                                     Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine("Recipe divided/reset by 3 or triple.");
                                     Console.ResetColor();
@@ -219,7 +257,7 @@ namespace POEPart1
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Red; // If recipe is null run this error message
                             Console.WriteLine("Can't scale when there are no quantities to work with!");
                             Console.ResetColor();
                         }
@@ -228,7 +266,7 @@ namespace POEPart1
                     
                     case "clear":
                         
-                        if (recipe != null)
+                        if (recipe != null) 
                         {
                             Console.ForegroundColor = ConsoleColor.Cyan;
                             Console.WriteLine("Please confirm before clearing (yes, no)");
